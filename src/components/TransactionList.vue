2bf442bda026cb15eb794ac377321e5d2712ac6b
Anton T <antont@stacc.com>
2023-12-02 15:08:07 +0100

    testing options API for transactionlist

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,27 @@
 <template>
      <h3>History</h3>
       <ul id="list" class="list">
-        <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-          Cash <span>+$400</span><button class="delete-btn">x</button>
-        </li>
+       <li v-for="transaction in transactions"
+       :key="transaction.id"
+       :class="transaction.amount > 0 ? 'plus' : 'minus'"
+       >
+       {{transaction.text }}<span>$ {{transaction.amount}} </span>
+       <button class="delete-btn">x</button>
+       </li>
       </ul>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            transactions: [ 
+                { id: 1, text: 'Phone', amount: -199.99 },
+                { id: 2, text: 'Paycheck', amount: 500 },
+                { id: 3, text: 'Grandma money', amount: 100 },
+                { id: 4, text: 'Computer', amount: -399.99 }
+            ]
+        }
+    },
+}
+</script>
