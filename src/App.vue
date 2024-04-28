d3e03baa47e908e4c897ed55b8d8fa98ac9e6641
Anton T <antont@stacc.com>
2024-04-28 20:34:26 +0200

    add easy frontend template

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,13 @@
 <template>
- <div class="container">
-  <Header />
-  <Balance 
-  :totalProp="+getTotal"
-  />
-  <IncomeExpenses 
-  :incomeProp="+getIncome"
-  :expenseProp="+getExpenses"
-  />
-  <TransactionList
-  :transactionsProp="transactionsValues" 
-  @transactionDeleted="handleTransactionDeleted"
-  />
-  <AddTransactions 
-  @transactionsSubmitted="handleTransactionsSubmitted"
-  />
- </div>
+  <HeaderContent />
+  <MainContent />
+  <BottomContent />
 </template>
 
-
 <script setup>
-import Header from './components/Header.vue'
-import Balance from './components/Balance.vue'
-import IncomeExpenses from './components/IncomeExpenses.vue'
-import TransactionList from './components/TransactionList.vue'
-import AddTransactions from './components/AddTransactions.vue'
 
-import { ref, computed, onMounted } from 'vue'
-import { nanoid } from 'nanoid'
-import { useToast } from 'vue-toastification'
+import { defineProps } from 'vue'
 
-const toast = useToast()
-
-const transactionsValues = ref([])
-
-onMounted(() => {
-  const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
-
-if(savedTransactions){
-  transactionsValues.value = savedTransactions
-}
-
-})
-
-
-//start the index at 0
-const getTotal = computed(() => {
-  return transactionsValues.value.reduce((index, transaction) => {
-    return index + transaction.amount
-  }, 0)
-})
-
-const getIncome = computed(() => {
-  return transactionsValues.value
-  .filter((transaction) => transaction.amount > 0)
-  .reduce((index, transaction) => {
-  return index + transaction.amount
- }, 0).toFixed(2)
-})
-
-
-const getExpenses = computed(() => {
-  return transactionsValues.value
-  .filter((transaction) => transaction.amount < 0)
-  .reduce((index, transaction) => {
-  return index + transaction.amount
- }, 0).toFixed(2)
- })
-
-
- const handleTransactionsSubmitted = (transactionData) => {
-  transactionsValues.value.push({
-    id: nanoid(),
-    text: transactionData.text,
-    amount: transactionData.amount
-  })
-
-  saveTransactionsToLocalStorage()
-  toast.success('Transaction added')
- }
-
- const handleTransactionDeleted = (id) => {
-  transactionsValues.value = transactionsValues.value.filter(
-    (transaction) => transaction.id !== id
-  )
-
-  saveTransactionsToLocalStorage()
-  toast.success('Transaction deleted')
- }
-
-
- const saveTransactionsToLocalStorage = () => {
-  localStorage.setItem('transactions', JSON.stringify(transactionsValues.value))
- }
 
 </script>
+
--- src/main.js
+++ src/main.js
@@ -1,12 +1,44 @@
 
 import { createApp } from 'vue'
-
-import './assets/style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+           
+//Theme
+import 'primevue/resources/themes/aura-light-green/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
-import Toast from "vue-toastification"
-import "vue-toastification/dist/index.css"
+
+//Components
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';  
+import Row from 'primevue/row';   
+import Card from 'primevue/card';
+import Menubar from 'primevue/menubar';
+import Panel from 'primevue/panel';
+
+
+
+//Common
+import HeaderContent from './common/HeaderContent.vue';
+import MainContent from './common/MainContent.vue';
+import BottomContent from './common/MainContent.vue';
+
+
+
 
 const app = createApp(App)
-app.use(Toast)
+app.use(PrimeVue);
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+app.component('ColumnGroup', ColumnGroup)
+app.component('DataTable', DataTable)
+app.component('Row', Row)
+app.component('Card', Card)
+app.component('Menubar', Menubar)
+app.component('Panel', Panel)
+app.component('HeaderContent', HeaderContent)
+app.component('MainContent', MainContent)
+app.component('BottomContent', BottomContent)
 app.mount('#app')
